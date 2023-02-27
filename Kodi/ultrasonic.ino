a78957227c0cbba9355c8f0e8b165861d747fc37
Gunnar Þórunnarson <gus@tskoli.is>
2023-02-27 13:30:20 +0000

    Update ultrasonic.ino

--- Kodi/ultrasonic.ino
+++ Kodi/ultrasonic.ino
@@ -7,29 +7,37 @@
 
 const int ECHO = 2; 
 const int TRIG = 3; 
-
-// fall sem sér um mælinguna, útfært hér fyrir neðan
-// fallið skilar fjarlægt í cm.
-int fjarlaegd(); 
+int distance = 0;  
+int fjarlaegd();  // fall sem sér um fjarlægðamælinguna, skilar fjarlægð í cm. 
 
 void setup() {
+  
     Serial.begin(9600); 
-
     pinMode(TRIG,OUTPUT);
     pinMode(ECHO,INPUT);
+  
 }
 
 void loop () {
-
-    if(fjarlaegd() < 50) { // ef fjarlægð í næsta hluta er minna en 50 cm.
-        // setja sýninguna af stað
+    
+    distance = fjarlaegd()           // fáum fjarlægðamælingu í cm.
+    Serial.print("Fjarlaegd: ");     // til að sjá mælingu í skjánum
+    Serial.println(fjarlaegd());     // til að sjá mælingu í skjánum
+  
+    // fjarlægð getur ekki verið mínustala
+    if(distance < 0) { 
+      distance = 0;
     } 
-
-    Serial.print("Fjarlaegd: ");
-    Serial.println(fjarlaegd());
+  
+    // ef fjarlægð í hlut er minna en 50 cm, má ekki vera neikvæð tala.
+    if(distance < 50 && distance != 0) {  
+      // setja sýninguna af stað
+    } 
+   
 }
 
 int fjarlaegd() {
+  
     // sendir út púlsa
     digitalWrite(TRIG,LOW);
     delayMicroseconds(2); // of stutt delay til að skipta máli
@@ -39,4 +47,6 @@ int fjarlaegd() {
 
     // mælt hvað púlsarnir voru lengi að berast til baka
     return (int)pulseIn(ECHO,HIGH)/58.0; // deilt með 58 til að breyta í cm
+  
 }
+
