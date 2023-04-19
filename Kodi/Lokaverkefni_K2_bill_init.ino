1556fe91ea4bbbc91434bd3944595f651e2f26c7
gestskoli <ges@tskoli.is>
2023-04-19 09:46:25 +0000

    Update Lokaverkefni_K2_bill_init.ino

--- Kodi/Lokaverkefni_K2_bill_init.ino
+++ Kodi/Lokaverkefni_K2_bill_init.ino
@@ -13,46 +13,46 @@ int Bi1 = 7;
 int Bi2 = 8;
 
 void setup() {
-	// Sonic pinnar
-	pinMode(trigPin, OUTPUT);
-	pinMode(echoPin, INPUT);
-	// Mótor pinnar
-	pinMode(pwmA, OUTPUT);
-	pinMode(pwmB, OUTPUT);
-	pinMode(Ai1, OUTPUT);
-	pinMode(Ai2, OUTPUT);
-	pinMode(Bi1, OUTPUT);
-	pinMode(Bi2, OUTPUT);
+  // Sonic pinnar
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  // Mótor pinnar
+  pinMode(pwmA, OUTPUT);
+  pinMode(pwmB, OUTPUT);
+  pinMode(Ai1, OUTPUT);
+  pinMode(Ai2, OUTPUT);
+  pinMode(Bi1, OUTPUT);
+  pinMode(Bi2, OUTPUT);
 }
 
 void loop() {  
   
-	afram(200);
+  afram(200);
 
 }
 
 void afram(int hradi) {
-	// Set Motor A forward
-	digitalWrite(Ai1, HIGH);
-	digitalWrite(Ai2, LOW);
+  // Set Motor A forward
+  digitalWrite(Ai1, HIGH);
+  digitalWrite(Ai2, LOW);
 
-	// Set Motor B forward
-	digitalWrite(Bi1, HIGH);
-	digitalWrite(Bi2, LOW);
+  // Set Motor B forward
+  digitalWrite(Bi1, HIGH);
+  digitalWrite(Bi2, LOW);
 
-	// Set the motor speeds
-	analogWrite(pwmA, hradi);
-	analogWrite(pwmB, hradi);
+  // Set the motor speeds
+  analogWrite(pwmA, hradi);
+  analogWrite(pwmB, hradi);
 }
 
 int maelaFjarlaegd()  {
-	// sendir út púlsar
-	digitalWrite(trigPin, LOW);
-	delayMicroseconds(2);
-	digitalWrite(trigPin, HIGH);
-	delayMicroseconds(10);
-	digitalWrite(trigPin, LOW);
+  // sendir út púlsar
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
 
-	// mælt hvað púlsarnir voru lengi að berast til baka
-	fjarlaegd = pulseIn(echoPin,HIGH)/58.0; // deilt með 58 til að breyta í cm
+  // mælt hvað púlsarnir voru lengi að berast til baka
+  fjarlaegd = pulseIn(echoPin,HIGH)/58.0; // deilt með 58 til að breyta í cm
 }
