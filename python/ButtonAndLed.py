3a1132fd69bd4a74513305a5103461ddbf054437
Gunnar Þórunnarson <gus@tskoli.is>
2023-08-27 14:04:41 +0000

    Update ButtonAndLed.py

--- python/ButtonAndLed.py
+++ python/ButtonAndLed.py
@@ -2,14 +2,12 @@ from machine import Pin
 
 led = Pin(2, Pin.OUT)
 
-#create button object from pin13,Set Pin13 to Input
-button = Pin(13, Pin.IN,Pin.PULL_UP) 
+#create button object from pin13, Set Pin13 to Input
+button = Pin(13, Pin.IN, Pin.PULL_UP) 
 
-try:
-    while True:
-      if not button.value():     
-        led.value(1)  #Set led turn on
-      else:
-        led.value(0)  #Set led turn off
-except:
-    pass
+while True:
+    if button.value():      # ef ekki er ýtt á takka þá er gildið 1 lesið.
+        led.value(0)  #led off
+    else:
+        led.value(1)  #led on
+
