90625e22cefd82aec76bea8066dbc4d60f909b93
Gunnar Þórunnarson <gus@tskoli.is>
2023-08-27 15:27:56 +0000

    Create ReedSwitch.py

---
+++ python/ReedSwitch.py
@@ -0,0 +1,14 @@
+from machine import Pin
+from time import sleep_ms
+
+led = Pin(2, Pin.OUT)
+snertiskynjari = Pin(13, Pin.IN, Pin.PULL_UP)   
+
+print(snertiskynjari.value()) 
+
+while True:
+    if snertiskynjari.value():      # ef reitur er tómur þá er gildið 1 lesið.
+        led.value(0)  # led off
+    else:
+        led.value(1)  # led on, þegar reitur er snertur.
+        print(snertiskynjari.value())
