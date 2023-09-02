f55816c7adb81b9787f91cfe34f16990b22accf9
Gunnar Þórunnarson <gus@tskoli.is>
2023-09-02 15:50:01 +0000

    Create Led_Print.py

---
+++ python/Led_Print.py
@@ -0,0 +1,17 @@
+from machine import Pin    # Notum Pin úr machine kóðasafninu til að skilgreina pinnana á ESP. 
+from time import sleep_ms  # Notum svo sleep_ms til að láta forritið bíða
+
+# Búum til breytuna led og tengjum hana við pinna 3 á ESP. 
+led = Pin(3, Pin.OUT)   # Segjum að pinni 3 sé úttakspinni.
+
+# Gerum lykkju sem keyrir að eilífu
+while True:
+    
+    led.value(1)     	  # Skrifum 3.3V út á pinna 3
+    print(led.value())	# Lesum stöðu á led með value aðferð og skrifum út í Shell
+    sleep_ms(2000)    	# Bíðum í 2 sekúndur
+
+    led.value(0)    	  # Skrifum 0V út á pinna 3
+    print(led.value())	# Lesum stöðu á led með value aðferð og skrifum út í Shell
+    sleep_ms(500)     	# Bíðum aftur í 500 millisekúndur
+
