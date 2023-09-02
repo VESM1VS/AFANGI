f7d3c95a41c0b8f357b712ad53a2c2be433b5ffd
Gunnar Þórunnarson <gus@tskoli.is>
2023-09-02 17:50:58 +0000

    Update 5_ActiveBuzzer.py

--- python/5_ActiveBuzzer.py
+++ python/5_ActiveBuzzer.py
@@ -3,13 +3,13 @@ from machine import Pin
 button = Pin(14,Pin.IN,Pin.PULL_UP)  # takki tengist pinna 14 og GND
 activeBuzzer = Pin(21,Pin.OUT)       # buzzer tengist pinna 21 og GND, active buzzer er með svart tape á botninum.
 
-activeBuzzer.value(0) 				 # höfum slökkt á active buzzer í upphafi,
+activeBuzzer.value(0)                # höfum slökkt á active buzzer í upphafi,
 
 while True:
-    if not button.value(): 			# ef ýtt er á takka
-        activeBuzzer.value(1)  		# hljóð á
+    if not button.value():           # ef ýtt er á takka
+        activeBuzzer.value(1)  	     # hljóð á
     else:
-        activeBuzzer.value(0)  		# hljóð af
+        activeBuzzer.value(0)        # hljóð af
         
         
 """
