4944a28c95f0e08a9583df7000ac2d4dc3ffc6af
Gunnar Þórunnarson <gus@tskoli.is>
2023-09-02 16:02:23 +0000

    Update Blink.py

--- python/Blink.py
+++ python/Blink.py
@@ -17,3 +17,13 @@ while True:
     led.value(0)
     # Bíðum aftur í 500 millisekúndur
     sleep_ms(500)
+
+"""
+Fallið value() er notað til að lesa eða skrifa á pinnann. Dæmi:
+    led.value(0) 		# slekkur á pinnanum
+    led.value(1) 		# kveikir á pinnanum
+    led.value(-42) 		# kveikir á pinnanum
+    led.value(False) 	# slekkur á pinnanum
+    
+    stada = led.value() # les hvort ledstaða er 0 eða 1, les þegar sviginn er tómur.
+"""
