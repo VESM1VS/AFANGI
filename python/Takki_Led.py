76e646ead59c712dc9c1ef297a727a7ab7db076b
Gunnar Þórunnarson <gus@tskoli.is>
2023-09-02 17:15:48 +0000

    Update and rename ButtonAndLed.py to Takki_Led.py

---
+++ python/Takki_Led.py
@@ -0,0 +1,11 @@
+from machine import Pin
+
+led = Pin(2, Pin.OUT)
+takki = Pin(14, Pin.IN, Pin.PULL_UP)	# vír tengdur í GND, upphafsstaða er 1
+
+while True:
+    if not takki.value():       # ef ýtt er á takka þá er gildið 0 lesið af takka
+        led.value(1)			# led on
+    else:
+        led.value(0)  			# led off, ekki ýtt á takka eða takka sleppt
+        
