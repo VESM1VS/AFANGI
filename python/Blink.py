adb4143e81491bd62b764e8e726e882c13ce3962
Gunnar Þórunnarson <gus@tskoli.is>
2023-09-02 15:07:24 +0000

    Update Blink.py

--- python/Blink.py
+++ python/Blink.py
@@ -1,11 +1,19 @@
-from time import sleep_ms
+# Notum Pin úr machine kóðasafninu til að skilgreina pinnana á ESP. 
 from machine import Pin
+# Notum svo sleep_ms til að láta forritið bíða
+from time import sleep_ms
 
-led=Pin(2,Pin.OUT) #create LED object from pin2,Set Pin2 to output
+# Búum til breytuna led og tengjum hana við pinna 2 á ESP. Segjum að pinninn sé úttakspinni
+# Pinni 2 er tengdur við led peru sem er innbyggð í ESP
+led = Pin(2, Pin.OUT)
 
+# Gerum lykkju sem keyrir að eilífu
 while True:
-    led.value(1)        #Set led turn on
-    sleep_ms(1000)
-    led.value(0)        #Set led turn off
-    sleep_ms(1000)
-
+    # Skrifum 3.3V út á pinna 2
+    led.value(1)
+    # Bíðum í 500 millisekúndur
+    sleep_ms(500)
+    # Skrifum 0V út á pinna 2
+    led.value(0)
+    # Bíðum aftur í 500 millisekúndur
+    sleep_ms(500)
