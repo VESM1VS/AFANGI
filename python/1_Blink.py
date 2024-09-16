70256ea83ffe688cfaaadeb0f8b8a1b85fecb3e8
Gunnar Þórunnarson <gus@tskoli.is>
2024-09-16 17:21:06 +0000

    Update 1_Blink.py

--- python/1_Blink.py
+++ python/1_Blink.py
@@ -9,17 +9,10 @@ led = Pin(2, Pin.OUT)     # Pinni 2 er tengdur við led peru sem er innbyggð í
 
 # Gerum lykkju sem keyrir að eilífu
 while True:
-    led.value(1)        # Skrifum 3.3V út á pinna 2
+    led.value(1)        # Skrifum 3.3V út á pinna 2. Fallið value() er notað til að skrifa á pinnann. 
     sleep_ms(500)       # Bíðum í 500 millisekúndur
     led.value(0)        # Skrifum 0V út á pinna 2
     sleep_ms(500)       # Bíðum aftur í 500 millisekúndur
 
-"""
-Fallið value() er notað til að lesa eða skrifa á pinnann. Dæmi:
-    led.value(0) 		# slekkur á pinnanum
-    led.value(1) 		# kveikir á pinnanum
-    led.value(-42) 		# kveikir á pinnanum
-    led.value(False) 	# slekkur á pinnanum
-    
-    stada = led.value() # les hvort ledstaða er 0 eða 1, les þegar sviginn er tómur.
-"""
+
+
