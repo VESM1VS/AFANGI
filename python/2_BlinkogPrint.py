faf7b6a6c96a7337f455c55b298341ece353eda7
Gunnar Þórunnarson <gus@tskoli.is>
2024-09-18 11:26:52 +0000

    Update 2_BlinkogPrint.py

--- python/2_BlinkogPrint.py
+++ python/2_BlinkogPrint.py
@@ -1,10 +1,11 @@
 # Blátt LED blikkar með 2 sek. millibili og skrifar út 0 og 1 á víxl í Shell í Thonny
+# Tengingar og kóði: https://wokwi.com/projects/409363760976600065
 
 from machine import Pin    # Notum Pin úr machine kóðasafninu til að skilgreina pinnana á ESP. 
 from time import sleep_ms  # Notum svo sleep_ms til að láta forritið bíða
 
 # Búum til breytuna led og tengjum hana við pinna 3 á ESP. 
-led = Pin(4, Pin.OUT)   # Segjum að pinni 4 sé úttakspinni.
+led = Pin(4, Pin.OUT)   # Segjum að pinni 3 sé úttakspinni.
 
 # Gerum lykkju sem keyrir að eilífu
 while True:
