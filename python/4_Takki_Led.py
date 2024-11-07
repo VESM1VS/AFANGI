ef1b4fea3fb35286b81407b4e8fceb9b86d9baad
Gunnar Þórunnarson <gus@tskoli.is>
2024-11-07 14:05:58 +0000

    Update 4_Takki_Led.py

--- python/4_Takki_Led.py
+++ python/4_Takki_Led.py
@@ -1,11 +1,13 @@
+# Takki sem kveikir á led
+# Tengingar og kóði: https://wokwi.com/projects/409365625870394369
+
 from machine import Pin
 
 led = Pin(4, Pin.OUT)
-takki = Pin(2, Pin.IN, Pin.PULL_UP)	# vír tengdur í GND, upphafsstaða er 1
+takki = Pin(2, Pin.IN, Pin.PULL_UP)	 # vír tengdur í GND, upphafsstaða er 1
 
 while True:
     if not takki.value():   # ef ýtt er á takka þá er gildið 0 lesið af takka
         led.value(1)        # led on
     else:
         led.value(0)        # led off (ekki ýtt á takka eða takka sleppt)
-        
