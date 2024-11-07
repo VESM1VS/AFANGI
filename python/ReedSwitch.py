ee8661889d1f38d8442ad21d2ca1e19314899629
Gunnar Þórunnarson <gus@tskoli.is>
2024-11-07 14:06:40 +0000

    Update ReedSwitch.py

--- python/ReedSwitch.py
+++ python/ReedSwitch.py
@@ -1,11 +1,13 @@
+# ReedSwitch sem virkar með segul
+# Passa að beygla ekki ReedSwitch vírana (brotna auðveldlega)
 from machine import Pin
 from time import sleep_ms
 
-led = Pin(2, Pin.OUT)
-snertiskynjari = Pin(13, Pin.IN, Pin.PULL_UP)    # nota líka aukalega resistor
+led = Pin(4, Pin.OUT)
+reed = Pin(2, Pin.IN, Pin.PULL_UP)    
 
 while True:
-    if snertiskynjari.value():      # ef reitur er tómur þá er gildið 1 lesið.
-        led.value(0)  # led off
+    if reed.value():      # ef reitur er tómur þá er gildið 1 lesið.
+        led.value(0)      # led off
     else:
-        led.value(1)  # led on, þegar reitur er snertur.
+        led.value(1)      # led on, þegar reitur er snertur.
