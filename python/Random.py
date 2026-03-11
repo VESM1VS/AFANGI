36d365e2616aa1df940872ec67b335f0d6f85895
Gunnar Þórunnarson <gus@tskoli.is>
2026-03-11 15:05:49 +0000

    Update Random.py

--- python/Random.py
+++ python/Random.py
@@ -1,17 +1,22 @@
+# Wokwi: https://wokwi.com/projects/452533563326722049
+
 from machine import Pin
 from random import randint  # til að geta unnið með random
+from time import sleep_ms 
 
 led1 = Pin(2, Pin.OUT)
-led2 = Pin(16, Pin.OUT) 
-led3 = Pin(17, Pin.OUT) 
+led2 = Pin(4, Pin.OUT) 
+led3 = Pin(5, Pin.OUT) 
 
-pins = [led1, led2, led3]	# notum lista til að halda utan um mörg gildi.
-
-randomLed = randint(0, 2)   # random skilar gildinu 0, 1 eða 2.
-print(randomLed)            # skrifum random gildið í Shell.
-pins[randomLed].value(1)    # Skrifum 3.3V út á pinna 2, 16 eða 17.
+# notum lista til að halda utan um mörg gildi.
+pins = [led1, led2, led3]	
 
 while True:
-    pass	# gerir ekkert
+    randomLed = randint(0, 2)   # random skilar gildinu 0, 1 eða 2.
+    print(randomLed)            # skrifum random gildið í Shell.
+    pins[randomLed].value(1)    # Skrifum 3.3V út á pinna 2, 5 eða 6.
+    sleep_ms(1000)              # Leyfum því að loga í 1 sek 
+    pins[randomLed].value(0)    # slökkvum á led
 
-# ath. þú þarf að útfæra betri lausn þannig að aðeins eitt led logi í einu.
+
+# nánar um lista: https://www.w3schools.com/python/python_lists.asp
