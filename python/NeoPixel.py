9cbb177ffc4dde4f65b377b715bceaae31d208e6
Gunnar Þórunnarson <gus@tskoli.is>
2026-03-11 15:08:37 +0000

    Update NeoPixel.py for brightness and comments

--- python/NeoPixel.py
+++ python/NeoPixel.py
@@ -1,3 +1,4 @@
+# https://wokwi.com/projects/441804604337655809
 from machine import Pin
 import neopixel
 import time
@@ -6,10 +7,14 @@ import time
 # S digital pinni
 # V 5V
 # G GND
+
 pin = Pin(48, Pin.OUT)
 np = neopixel.NeoPixel(pin, 8)	# 8 x RGB Leds
 
-brightness=100                  # birtustig: 0 - 255
+# breytur
+brightness = 255                # birtustig frá 0 - 255
+
+# búum til liti með RGB litakerfi.
 red   =  [ brightness, 0, 0]    # red
 green =  [ 0, brightness, 0]    # green  
 blue  =  [ 0, 0, brightness]    # blue
@@ -17,27 +22,27 @@ off   =  [ 0, 0, 0]             # ekkert ljós
 
 while True:
     # stjórnum öllum 8 leds í einu með að nota fill
-    np.fill([0, 0, 0])
+    np.fill([0, 0, 0])     # ekkert ljós
     
-    np[0] = red		# led 1, np[0] = [ 255, 0, 0]  
-    np.write()      # kveikjum á led 1
+    # led 1  
+    np[0] = red		        # eða np[0] = [255, 0, 0] 
+    np.write()              # kveikjum á led 1
+    time.sleep_ms(500)      # hinkrum í hálfa sek. þar til við gerum eitthvað annað.
+
+    # led 2
+    np[1] = green	        
+    np.write()
     time.sleep_ms(500)
-    
-    np[1] = green	# led 2
+
+    # led 3
+    np[2] = blue	       
     np.write()
     time.sleep_ms(500)
     
-    np[2] = blue	# led 3
-    np.write()
-    time.sleep_ms(500)
-    
-    np[0] = off
+    np[0] = off            
     np[1] = off
     np[2] = off
     np.write()	
 
     time.sleep_ms(1000) 
     
-
-    
-
